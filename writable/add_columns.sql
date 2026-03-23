1cb875e770177a19a358e826a776563f20bc5a76
JhondellRanises <projhondell@gmail.com>
2026-03-23 22:11:18 +0800

    Remove unnecessary files

--- writable/add_columns.sql
+++
@@ -1,5 +0,0 @@
--- Add flavor columns to products table
-ALTER TABLE products 
-ADD COLUMN flavor VARCHAR(100) NULL AFTER brand,
-ADD COLUMN flavor_category VARCHAR(50) NULL AFTER flavor,
-ADD COLUMN puffs INT NULL AFTER flavor_category;
